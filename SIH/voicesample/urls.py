6fe5d554cdc741de2f2e9387819cf5ecdc2e8691
pavankomarina <komarina.pavankalyan@gmail.com>
2018-03-19 23:48:27 +0530

    added home page

--- SIH/voicesample/urls.py
+++ SIH/voicesample/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from . import views
-
+from .views import *	
 app_name = 'voicesample'
 
 urlpatterns = [
+ 	url(r'^$', views.index, name='index'),
     ]
 
 
--- SIH/voicesample/views.py
+++ SIH/voicesample/views.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404,redirect,HttpResponseRedirect
 from .models import *
 # Create your views here.
+
+
+def index(request):
+	return render(request,'index.html',{})
