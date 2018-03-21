91c894c2fdbc75102aaef05a7d6f42c3cad61cc6
naela tarannum <naela16@gmail.com>
2018-03-21 07:53:01 -0700

    commit

--- SIH/voicesample/urls.py
+++ SIH/voicesample/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
 from . import views
-from .views import *	
+from .views import *
 app_name = 'voicesample'
 
 urlpatterns = [
  	url(r'^$', views.index, name='index'),
+    url(r'^loginpage$',views.loginpage,name='loginpage'),
     ]
-
-
